313c12892ff923fd93126b169a617fcf25f87b92
web3 <weelam939@gmail.com>
2026-03-26 02:57:03 +0800

    Update fresh_bot23.py

--- fresh_bot23.py
+++ fresh_bot23.py
@@ -138,7 +138,7 @@ def record_orderbook(asset, yes_price, no_price):
 
 DRY_RUN         = False
 ASSETS          = ["btc", "eth", "sol"]
-BUY_AMOUNT      = 2       # USDC to spend per trade
+BUY_AMOUNT      = 5       # USDC to spend per trade
 BUY_PRICE_MIN   = 0.80    # Buy if price >= 82c
 BUY_PRICE_MAX   = 0.84    # Buy if price <= 84c
 SELL_PRICE      = 0.97    # Sell main shares at 97c
