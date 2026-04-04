f05a394b6ac490b196b25f510486f0a3b0450231
web3 <weelam939@gmail.com>
2026-04-04 13:43:28 +0800

    Disable trading windows by setting TRADING_WINDOWS_ENABLED to False

--- panic_bot6.py
+++ panic_bot6.py
@@ -153,7 +153,7 @@ STOP_TRADE_SECS  = 810    # stop opening NEW trades after this many seconds into
 # --- Optional trading windows (entry only; exits always allowed) -------------
 # Leave TRADING_WINDOWS_ENABLED=False to trade anytime.
 # Example: trade only 8pm-10pm local time -> TRADING_WINDOWS_ENABLED=True; TRADING_WINDOWS=[(20, 22)]
-TRADING_WINDOWS_ENABLED = True
+TRADING_WINDOWS_ENABLED = False
 TRADING_TZ_OFFSET_HRS   = 8      # local timezone offset from UTC
 TRADING_WINDOWS         = [(9, 11), (20, 22), (0, 1)]     # list of (start_hour, end_hour), end is exclusive
 
